e36c6ccfcedf9c2a563a91e7014b7050bc60ad39
zhaoyan <499492253@qq.com>
2024-07-14 17:26:28 +0800

    暂存

--- Example/ZLYYSwift/ViewController.swift
+++ Example/ZLYYSwift/ViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright (c) 2024 499492253@qq.com. All rights reserved.
 //
 
+import ZLYYSwift
 import UIKit
 
 class ViewController: UIViewController {
@@ -13,11 +14,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Date().days
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
 
 }
---
+++ ZLYYSwift/Classes/Test.swift
@@ -0,0 +1,14 @@
+//
+//  Test.swift
+//  ZLYYSwift
+//
+//  Created by ZY on 2024/7/14.
+//
+
+import UIKit
+
+class Test: NSObject {
+    func testLog() {
+        print("Test")
+    }
+}
